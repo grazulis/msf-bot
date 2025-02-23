65946a0dda954ee61753754c158d9a10ba4cc980
Gary Leeming <gary.leeming@gmail.com>
2025-02-23 23:41:31 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,20 +1,32 @@
-from flask import Flask
+import os
 
-# Flask constructor takes the name of 
-# current module (__name__) as argument.
+from flask import (Flask, redirect, render_template, request,
+                   send_from_directory, url_for)
+
 app = Flask(__name__)
 
-# The route() function of the Flask class is a decorator, 
-# which tells the application which URL should call 
-# the associated function.
+
 @app.route('/')
-# ‘/’ URL is bound with hello_world() function.
-def hello_world():
-    return 'Hello World'
+def index():
+   print('Request for index page received')
+   return render_template('index.html')
 
-# main driver function
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'),
+                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
+
+@app.route('/hello', methods=['POST'])
+def hello():
+   name = request.form.get('name')
+
+   if name:
+       print('Request for hello page received with name=%s' % name)
+       return render_template('hello.html', name = name)
+   else:
+       print('Request for hello page received with no name or blank name -- redirecting')
+       return redirect(url_for('index'))
+
+
 if __name__ == '__main__':
-
-    # run() method of Flask class runs the application 
-    # on the local development server.
-    app.run()
+   app.run()
